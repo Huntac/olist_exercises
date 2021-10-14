2c514ce6c50aaa80e6c69325227d0280514d2e86
Huntac <ch360409@gmail.com>
2021-10-14 11:41:35 -0400

    fixed bugs added various metrics
    added validation and training metrics for the best estimator fir during the grid search cross validation. Saves metrics and pickles model for evaluation with test set.

--- review_sentiment_analysis.py
+++ review_sentiment_analysis.py
@@ -1,6 +1,6 @@
 from olist_utils import get_olist_dataset
 from datetime import datetime
-
+import json
 import logging
 import matplotlib as plt
 import numpy as np
@@ -26,7 +26,7 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import fbeta_score, make_scorer, classification_report
 
 import sys
-import yaml
+
 
 # globals
 STOPWORDS = stopwords.words('portuguese')
@@ -52,6 +52,8 @@ class StringProcessing(BaseEstimator, TransformerMixin):
             text = re.sub(r'\d', ' ', text)
             # remove punctuation
             text = re.sub(r'[^\w\s]', ' ', text)
+            # remove words with more than 20 characters
+            text = re.sub(r'\b\w{21,}\b', '', text)
             # change multiple spaces to single space
             text = re.sub(r'\s+', ' ', text)
             # remove white space
@@ -95,7 +97,7 @@ class StemProcessing(BaseEstimator, TransformerMixin):
 if __name__ == '__main__':
 
 
-    runtime = datetime.now().strftime('(%m-%d-%Y_%H:%M:%S)')
+    runtime = datetime.now().strftime('(%m-%d-%Y_%H%M%S)')
     #sets up logging
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -119,7 +121,7 @@ if __name__ == '__main__':
         reviews_df = reviews_df.dropna()
 
         # Assign label of 0 if review score < 3, 1 otherwise
-        reviews_df['label'] = pd.cut(reviews_df['review_score'], bins = [0,2,5], labels = [0,1]) 
+        reviews_df['label'] = pd.cut(reviews_df['review_score'], bins = [0,2,5], labels = [1,0]) 
         reviews_df.drop(columns = ['review_score'], inplace = True)
 
         y = reviews_df['label']
@@ -141,16 +143,14 @@ if __name__ == '__main__':
             ('string_manip', StringProcessing()),
             ('stopwords', StopwordProcessing()),
             ('stemming', StemProcessing()),
-            ('bow', CountVectorizer(stop_words=STOPWORDS)),
+            ('bow', CountVectorizer(stop_words=STOPWORDS, ngram_range=(1,1))),
             ('tfidf', TfidfTransformer()),
             ('rf_clf', RandomForestClassifier(class_weight='balanced', random_state = RANDOM_STATE))
         ])
 
         params = {
-            'bow__max_df': (0.5, 0.75, 1.0),
-            'bow__ngram_range': ((1,1), (1,2)),
-            'tfidf__norm': ('l1', 'l2'),
-            'rf_clf__n_estimators': (100, 300, 600),
+            'bow__max_df': (0.15, 0.35, 0.5),
+            #'rf_clf__n_estimators': (100, 300),
             'rf_clf__max_depth': (3, 5)
         }
 
@@ -158,35 +158,38 @@ if __name__ == '__main__':
         beta = 2
         scorer = make_scorer(fbeta_score, beta = beta)
 
-        grid = GridSearchCV(tfidf_class_pipe, params, scoring = scorer, verbose = 2)
+        grid = GridSearchCV(tfidf_class_pipe, params, scoring = scorer, verbose = 3, cv = 3)
         logger.info('Performing grid search')
         grid.fit(X_train, y_train)
 
         logger.info('Evaluating Model on Validation Set')
         val_preds = grid.best_estimator_.predict(X_val)
+        train_preds = grid.best_estimator_.predict(X_train)
 
         # Model Validation Metrics
-        val_report = classification_report(y_val, val_preds)
+        val_report = {'validation_metrics': classification_report(y_val, val_preds, output_dict = True),
+                      'training_metrics': classification_report(y_train, train_preds, output_dict = True)}
 
         val_report['fbeta_score'] = {'beta': beta,
                                      'score': fbeta_score(y_val, val_preds, beta = beta)}
 
         # Collect feature importances if available          
         feat_imp = None
-
+        feat_names = None
         if hasattr(grid.best_estimator_.named_steps['rf_clf'], 'feature_importances_'):
-            feat_imp = grid.best_estimator_.named_steps[name].feature_importances_
-        val_report['feature_importances'] = feat_imp
+            feat_imp = grid.best_estimator_.named_steps['rf_clf'].feature_importances_
+            feat_names = grid.best_estimator_.named_steps['bow'].get_feature_names()
+        val_report['feature_importances'] = dict(zip(feat_names,feat_imp))
 
         report_file = f'rf_clf_validation_report_{runtime}.json'
         model_file = f'rf_clf_model_{runtime}.pkl'
-        model_path = os.path.join('sentiment_analysis', 'models', model_file)
-        report_path = os.path.join('sentiment_analysis', 'results', report_file)
+        model_path = os.path.join('.','sentiment_analysis', 'models', model_file)
+        report_path = os.path.join('.','sentiment_analysis', 'reports', report_file)
 
         with open(model_path, 'wb') as f:
             pickle.dump(grid.best_estimator_, f)
 
-        with open(report_path, 'wb') as f:
+        with open(file = report_path, mode ='w') as f:
             json.dump(val_report, f)
 
     except Exception as e:
